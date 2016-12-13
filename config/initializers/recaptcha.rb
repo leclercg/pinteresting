9893b2a3e57ae1e4ffd750a2937cf9440f4b0eed
Georges Leclerc <georges.leclerc@gmail.com>
2016-12-12 22:48:27 -0500

    Adding OpenSSL PKey info

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = Rails.application.secrets.recaptcha_public_key
+  config.public_key  = OpenSSL::PKey::EC.new Rails.application.secrets.recaptcha_public_key
   config.private_key = Rails.application.secrets.recaptcha_private_key
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
