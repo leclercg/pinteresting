878e55b60cf548f5b87f8ce6597ac834812c4332
Georges Leclerc <georges.leclerc@gmail.com>
2016-12-26 15:47:21 -0500

    Fixing issue showing all pins for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @pin_count = @user.pins.count
-    @user.pins = @user.pins.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+    @pins = @user.pins.all
+    @user.pins = @pins.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
 
   end
 
